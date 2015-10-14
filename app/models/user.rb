70bd0b03ad9ce2075247ba9829b1853f00e77643
chaiwen <cc3636@columbia.edu>
2015-10-13 20:43:32 -0400

    users have followers, and a list of users they follow

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,24 +6,28 @@ class User
   #has_many		:uploaded_photo, class_name: "Photo", inverse_of: :photoable
   #belongs_to	:liked_photo, class_name: "Photo", inverse_of: :photoable
   
-  has_and_belongs_to_many	:users							# friends list
+  #has_many	:friendships					# friends list
   
+  has_and_belongs_to_many :followers, class_name: "User", inverse_of: :following
+  has_and_belongs_to_many :following, class_name: "User", inverse_of: :followers
+
   has_many		:activities 								# author of Activity
-  has_many		:saved_activities, class_name: "Activity" 	# bookmarked Activities
+  has_many		:saved_activities, class_name: "Activity", inverse_of: :savers 	# bookmarked Activities
 
   has_many		:trips 										# author of Trip
-  has_many		:saved_trips, class_name: "Trip" 			# bookmarked Trips
+  has_and_belongs_to_many :saved_trips, class_name: "Trip", inverse_of: :savers			# bookmarked Trips
 
   has_many		:reviews, as: :reviewable 					# reviews the user wrote
   has_many		:photos										# uploaded photos
 
   # liked photo, part of liker list in photo
-  belongs_to	:photo, inverse_of: :liker # maybe don't need? 
+  # because it's nil in photo. belongs_to	:photo, inverse_of: :liker # maybe don't need? 
 
   #############################################################################
 
   field :first_name, type: String
   field :last_name, type: String
+  field :user_name, type: String
   field :email, type: String
   field :password, type: String
 
@@ -32,20 +36,22 @@ class User
   field :photo_date, type: String
 
 
+  # custom validation
+  validate :email_and_user_name_unique
+  def email_and_user_name_unique
+  	if email == nil
+  		errors.add(:email, "email required!")
+  	end
 
+  	if user_name == nil
+  		errors.add(:user_name, "user_name required and must be unique!")
+  	end
+  end
 
   # update view
 =begin
-  # custom validation example, mongoid
-  validate :first_name_last_name_same_letter
 
-  def first_name_last_name_same_letter
-  	if first_name && last_name
-  		if first_name.first != last_name.first
-  			errors.add(:first_name, "Needs to have same first letter as last name.")
-  		end
-  	end
-  end
+  
 
   before_create :send_message
   after_save do |document|
