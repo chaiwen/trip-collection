6da5895a531e9a694939fd86c8651c9dbfc171e6
chaiwen <cc3636@columbia.edu>
2015-10-27 20:04:26 -0400

    session store user.id.to_s fixes auto bson obj error

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,7 +1,7 @@
 module SessionsHelper
 	# Logs in the given user.
 	def log_in(user)
-		session[:user_id] = user.id # places temp cookie on user's browser containing encrypted version of user id
+		session[:user_id] = user.id.to_s # places temp cookie on user's browser containing encrypted version of user id
 		# temp cookie expires immediately when browser is closed
 
 		puts "USER LOGGED IN!!!!"
